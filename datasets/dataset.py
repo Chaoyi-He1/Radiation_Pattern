ddd1ff8a528559441d4eb08d1baebb821832e82c
Chaoyi He <chaoyi_he@tamu.edu>
2024-03-06 16:33:39 -0600

    modified:   datasets/dataset.py

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -11,8 +11,9 @@ class Pointnet_Dataset(Dataset):
                  num_points: int = 361, cache: bool = False,
                  split: float = 0.8):
         self.data_folder_path = data_folder_path
-        self.data_files_path = [os.path.join(data_folder_path, f) for f in os.listdir(data_folder_path) if f.endswith('.csv')]
+        self.data_files_path_az = [os.path.join(data_folder_path, 'az', f) for f in os.listdir(os.path.join(data_folder_path, 'az')) if f.endswith('.csv')]
         self.data_files_path.sort()
+        self.data_files_path_vt = [f.replace("az", "vt") for f in self.data_files_path_az]
         self.is_train = is_train
         self.split = split * len(self.data_files_path)
         self.data_files_path = self.data_files_path[:int(self.split)] if is_train else self.data_files_path[int(self.split):]
@@ -36,7 +37,9 @@ class Pointnet_Dataset(Dataset):
         if self.cache:
             data = self.data[index]
         else:
-            data = pd.read_csv(self.data_files_path[index], header=None).to_numpy()
+            data_az = pd.read_csv(self.data_files_path_az[index], header=None).to_numpy()
+            data_vt = pd.read_csv(self.data_files_path_vt[index], header=None).to_numpy()
+            data = np.concatenate((data_az, data_vt), axis=0)
         
         # Randomly selecting one point from every 10 points in data
         indices_1 = [random.randint(i*10, min((i+1)*10 - 1, data.shape[0]-1)) for i in range(data.shape[0] // 10)]
@@ -46,9 +49,9 @@ class Pointnet_Dataset(Dataset):
         
         return data_sampled_1, data_sampled_2
     
-    def collate_fn(self, batch):
+    @staticmethod
+    def collate_fn(batch):
         data_sampled_1, data_sampled_2 = list(zip(*batch))
         data_sampled_1 = torch.tensor(np.array(data_sampled_1)).float()
         data_sampled_2 = torch.tensor(np.array(data_sampled_2)).float()
         return data_sampled_1, data_sampled_2
-        
