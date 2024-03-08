7de50241db7735e53155f3f160fb541b0b0f9d85
Chaoyi He <chaoyi_he@tamu.edu>
2024-03-08 14:00:33 -0600

    modified:   train_eval/train_eval_backbone.py

--- train_eval/train_eval_backbone.py
+++ train_eval/train_eval_backbone.py
@@ -73,3 +73,26 @@ def evaluate(model: torch.nn.Module, criterion: torch.nn.Module,
     metric_logger.add_meter('class_accuracy', SmoothedValue(window_size=1, fmt='{value:.2f}'))
     metric_logger.add_meter('loss', SmoothedValue(window_size=1, fmt='{value:.6f}'))
     header = 'Test:'
+    
+    for input_1, input_2 in metric_logger.log_every(data_loader, 50, header):
+        input_1 = input_1.to(device)
+        input_2 = input_2.to(device)
+        
+        with torch.cuda.amp.autocast(enabled=scaler is not None):
+            outputs = model(torch.cat((input_1, input_2), dim=0))
+            output_1, output_2 = torch.split(outputs, input_1.size(0), dim=0)
+            
+            loss, acc = criterion(output_1, output_2)
+        
+        # reduce values over all GPUs for logging purposes
+        loss_reduced = reduce_loss(loss)
+        acc_reduced = reduce_loss(acc)
+        
+        # Update metric logger
+        metric_logger.update(loss=loss_reduced.item())
+        metric_logger.update(class_accuracy=acc_reduced.item())
+        
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
